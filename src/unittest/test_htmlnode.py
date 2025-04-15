3273439ae7381b5d87e36ea965e07a44d5166901
Kamel77127 <mkamel77127@gmail.com>
2025-04-15 07:08:09 +0200

    ajout des fichiers htmlNodes + test

---
+++ src/unittest/test_htmlnode.py
@@ -0,0 +1,17 @@
+import unittest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+from htmlnode import HtmlNode
+
+class TestHtmlNode(unittest.TestCase):
+    
+    def test_props(self):
+        node = HtmlNode("<a>" , "http://kamel.fr", None ,{"href" : "http://kamel.fr", "target": "_blank"})
+        print(node)
+        
+
+
+if __name__ == "__main__":
+    unittest.main()
+        
