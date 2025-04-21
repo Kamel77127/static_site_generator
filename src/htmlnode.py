8ee77372149d23f388ac3923c0a2cdd66ca176fc
Kamel77127 <mkamel77127@gmail.com>
2025-04-21 18:24:28 +0200

    added some shhhhh

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,7 +19,7 @@ class HtmlNode():
        if self.props == None:
            return html_text
        for item in self.props:
-           html_text += f"{item}'{self.props[item]}'"
+           html_text += f"{item}:'{self.props[item]}' "
        return html_text
             
     def __repr__(self):
@@ -29,8 +29,8 @@ class HtmlNode():
 class LeafNode(HtmlNode):
     
     
-    def __init__(self, tag=None, value=None, children=None, props=None):
-        super().__init__(tag, value, children, props)   
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value = value, props=props)   
         
     def to_html(self):
         if self.value == None:
@@ -39,3 +39,22 @@ class LeafNode(HtmlNode):
             return f"{self.value}"
         else:
             return f"<{self.tag} {super().props_to_html()}>{self.value}</{self.tag}>"
+        
+    
+
+class ParentNode(HtmlNode):
+    
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children = children, props = props)
+    
+    def to_html(self):
+        result = f"<{self.tag}>"
+        if not self.tag:
+            raise ValueError("Tag needed")
+        elif not self.children:
+            raise ValueError("children needed")
+        else:
+            for item in self.children:
+               result += item.to_html()
+        return result + f"</{self.tag}>"
+        
--- src/unittest/test_textnode.py
+++ src/unittest/test_textnode.py
@@ -1,14 +1,18 @@
 import unittest
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../src")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../src/utility")))
 from textnode import TextNode,TextTypes
-
+from utility import text_node_to_html_node,split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     
     def test_eq(self):
-        node1 = TextNode("this is a text ndoe", TextTypes.BOLD)
-        node2 = TextNode("this is a text ndoe", TextTypes.BOLD)
-        self.assertEqual(node1,node2)
+        node1 = TextNode("_this_ is a bold text node", TextTypes.TEXT)
+        node2 = TextNode("this _is_ a link ndoe", TextTypes.TEXT)
+        node4 = TextNode("this _is an image_", TextTypes.TEXT)
+        print(split_nodes_delimiter([node1,node2,node4],"_",TextTypes.ITALIC))
         
         
 if __name__ == "__main__":
